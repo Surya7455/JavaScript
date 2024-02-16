cac04ab94569b1cd9ab8b8d00632e563df69ddbc
Surya7455 <surya.pratap.cs26@iilm.edu>
2024-02-17 00:23:04 +0530

    datatype

---
+++ basic1.js
@@ -0,0 +1,19 @@
+console.log("Hello World")
+const accountId =12345
+let accountEmail ="ksunvng@gmail.com"
+var accountPassword = "5432" // var is not used yet besuse of some issuse 
+accountCity ="jhghdh"
+
+//const accountId = "12345678"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
+
+// Commit for single line
+/* Commit for
+multi lines*/
+
+
+
+
---
+++ basic2.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(4+5) //we are using nodejs, not browser
+
+console(3+4);
+
+let name ="kunal"
+let age =20
+let isLoggedIn = false
+
+// number =>2 to power 53
+//bigint
+//string =>""
+//boolean => ture/false
+//null=>standslone value //null is object
+// undefined =>
+//symbol=> unique
+
+
+//object
+
+
