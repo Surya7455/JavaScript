b10430c0164de6b387f56f8fda1da58a3de8db6e
Surya7455 <surya.pratap.cs26@iilm.edu>
2024-02-21 00:14:21 +0530

    Conversation and Comparison

---
+++ conversation.js
@@ -0,0 +1,29 @@
+//let score ="33jhg"
+let score = null
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+// "33" => 33
+// "33jhg" =>NaN
+// ture => 1; false =>0
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn )
+
+let num =3
+let negativenum = - num;
+console.log(num);
+
+console.log(2+2);
+console.log("2" + "2");
+console.log("2" + 1 );
+console.log(1 + 2 +"2")
+console.log("2" + 1 +3)
+
+
