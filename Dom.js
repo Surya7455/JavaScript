e0e0d48354946f7a232010583d1ea34572a819d0
Surya7455 <surya.pratap.cs26@iilm.edu>
2024-02-26 23:12:32 +0530

    Document(DOM)

---
+++ Dom.js
@@ -0,0 +1,20 @@
+/**Window 
+ * ---->>Document 
+ * ---->>HTML
+ * 1--->>>HEAD
+ * 1.1-->>>>title
+ * 1.1.A->text node
+ * 1.2-->>>>meta
+ * 1.2.A-> attribute
+ * 2--->>>BODY
+ * 2.1-->>>>>div 
+ * 2.1.A->h1
+ * 2.1.A.1->attribute
+ * 2.1.A.2->text node
+ * 2.2.B->p
+ * 2.2.B.1->text node
+*/
+// const parent = document.querySelector('_parent')
+// console.log(parent);
+// console.log(parent.childern);
+// console.log(parent.children[1].innerHTLM);
